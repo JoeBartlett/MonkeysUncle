e0aa8297c176efcd06633782f37d4a74d42a0f71
joebartlett <joe@avg-joe.com>
2017-06-08 23:48:04 -0500

    Fixing some windows errors

--- app/base.js
+++ app/base.js
@@ -2,11 +2,11 @@ require('jquery');
 require('jquery-ui');
 require('bootstrap');
 
-// require('./_scss/a-main.scss');
-// require('./_scss/a-user.scss');
-require('./_scss/partials/global');
-require('./_scss/partials/page');
-require('./_scss/partials/focus');
+// require('./scss/a-main.scss');
+// require('./scss/a-user.scss');
+require('./scss/partials/global');
+require('./scss/partials/page');
+require('./scss/partials/focus');
 
 //components
-require('./_scss/partials/components');
+require('./scss/partials/components');
