7470759697d669645499b8f4506b96309ca8c35e
joebartlett <joe@avg-joe.com>
2017-06-14 09:56:42 -0500

    More updates for frontnend

--- app/base.js
+++ app/base.js
@@ -5,8 +5,11 @@ require('bootstrap');
 // require('./scss/a-main.scss');
 // require('./scss/a-user.scss');
 require('./scss/partials/global');
+require('./scss/partials/header');
+require('./scss/partials/hero');
 require('./scss/partials/page');
-require('./scss/partials/focus');
+require('./scss/partials/footer');
+//require('./scss/partials/focus');
 
 //components
 require('./scss/partials/components');
